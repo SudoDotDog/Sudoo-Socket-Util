44f943bee59178df10efd3490348809003cc58dd
WMXPY <wm@mengw.io>
2021-11-07 14:48:19 -0600

    add btoa

--- src/authorization.ts
+++ src/authorization.ts
@@ -4,9 +4,16 @@
  * @description Authorization
  */
 
-export const parseBasicAuthorizationToken = (username: string, password: string): string => {
+export const encodeBasicAuthorizationTokenNode = (username: string, password: string): string => {
 
     const base64: string = Buffer.from(`${username}:${password}`).toString('base64');
+
+    return base64;
+};
+
+export const encodeBasicAuthorizationTokenWeb = (username: string, password: string): string => {
+
+    const base64: string = window.btoa(`${username}:${password}`);
 
     return base64;
 };
